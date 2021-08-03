ca98c9515bf90ad819da122f0dbe1e3c0b27741f
MarikoSekido <>
2021-08-03 17:22:16 +0900

    namespace, module,scopeなどなど

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -13,21 +13,50 @@ const handleHeartDisplay = (hasLiked) => {
   }
 }
 
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
+
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
-  axios.get(`/articles/${articleId}/comments`)
+  axios.get(`/api/articles/${articleId}/comments`)
     .then((response) => {
       const comments = response.data
       comments.forEach((comment) => {
-        $('.comments-container').append(
-          `<div class="article_comment"><p>${comment.content}</p></div>`
-        )
+        appendNewComment(comment)
       })
     })
+  
+  handleCommentForm()
 
-  axios.get(`/articles/${articleId}/like`)
+  $('.add-comment-button').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content){
+      window.alert('コメントを入力してください')
+    } else {
+      axios.post(`/api/articles/${articleId}/comments`, {
+        comment: {content: content}
+      })
+        .then((res) => {
+          const comment = res.data
+          appendNewComment(comment)
+          $('#comment_content').val('')
+        })
+    }
+  })
+
+  axios.get(`/api/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
